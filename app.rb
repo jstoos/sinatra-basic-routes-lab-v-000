ba5d0c5b0891960046d5887c3b4626790a9084ce
Jennifer Stoos <jstoos@bitlaw.com>
2018-06-01 23:34:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,15 +7,15 @@ class App < Sinatra::Base
   end
 
   get '/name' do
-    "My name is__"
+    "My name is "
   end
 
   get '/hometown' do
-    "My hometown is__"
+    "My hometown is "
   end
 
   get '/favorite-song' do
-    "My favorite song is__"
+    "My favorite song is "
   end
 
 end
